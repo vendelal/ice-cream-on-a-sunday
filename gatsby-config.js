09f2006d73be02f12b3ccb36a283d81bcbb3eaa2
vendelal <vendelalinnea@mac.com>
2019-07-01 14:51:38 -0400

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-143010707-1`,
+        anonymize: true,
       },
     },
     // `gatsby-plugin-feed`,
